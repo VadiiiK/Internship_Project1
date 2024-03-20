49dcfee8988903679484fc3c47f9617b08c7b047
Vadim <komissarcik@mail.ru>
2024-03-20 11:47:33 +0300

    Initial commit

---
+++ data_download.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+
+
+def fetch_stock_data(ticker, period='1mo'):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    return data
+
+
+def calculate_and_display_average_price(data):
+    data = data['Close'].mean()
+    return print(f'Средняя цена закрытия акций за указаный период равна: {data}')
+
